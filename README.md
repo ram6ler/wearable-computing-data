fc34fa4571d9722a47431760a909d5a7f12e4139
Richard Ambler <rambler@ibwya.net>
2014-05-25 14:02:02 +0800

    second commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Wearable computer data / readme
+
+This is a project that was done as part of the [Coursera](https://www.coursera.org) course, [Getting and Cleaning Data](https://www.coursera.org/course/getdata). For the project, we were given access to a really untidy data set, [Human Activity Recognition Using Smartphones](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones) from the [University of California, Irvine Machine Learning Repository](http://archive.ics.uci.edu/ml), from which we were to extract and organise some of the data. The raw data may be downloaded [here](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip).
+
+This repo consists of the following files:
+
+* __README.md__<br>This file, that you are reading.
+* __run_analysis.R__<br>The R code that produced the final, tidy data set.
+* __CODEBOOK.md__<br>An explanation of the variables in the final tidy data set.
---
+++ run_analysis.R
@@ -0,0 +1,101 @@
+setwd("/Users/rambler/Documents/Coursera/Data Science/03_GettingCleaningData/Project/UCI HAR Dataset")
+
+# Assumed that the working directory is "UCI HAR Dataset"
+# of the data exctracted from:
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+# Get the activity labels from "activity_labels.txt"
+activity.map <- read.table("activity_labels.txt")[, 2]
+
+# Get the original variable names.
+variable.names <- read.table("features.txt")[, 2]
+
+# Only mark the variables containing means or standard deviations.
+to.be.included <- grepl("(mean\\(\\))|(std\\(\\))", variable.names)
+
+# Create a function to clean up the names.
+CleanVar <- function(ugly.name) {
+  # According the requirements laid out around 10:00 into first
+  # lecture of week 4, except decided to keep the variable names 
+  # in camelCase format since some of the variable names were very 
+  # long. 
+  
+  ugly.name <- gsub("(mean\\(\\))", "Mean", ugly.name)
+  ugly.name <- gsub("(std\\(\\))", "Std", ugly.name)
+  
+  # To remove camelCase, we could simply uncomment the following line.
+  
+  # ugly.name <- tolower(ugly.name)
+  
+  # Only keep alphanumerical characters.
+  
+  ugly.name <- gsub("[^a-zA-Z0-9]", "", ugly.name)
+  
+  ugly.name
+}
+
+variable.names <- CleanVar(variable.names)
+
+# Create one data set by merging the training and test sets.
+
+final.selected <- data.frame(
+  subject = read.table("train/subject_train.txt")[, 1]
+)
+# Get the training data; this may take a few seconds.
+x.data <- read.table("train/X_train.txt", stringsAsFactors = FALSE)
+
+# Keep the data of interest.
+for (i in 1:561) {
+  if (to.be.included[i])
+    final.selected[variable.names[i]] <- x.data[, i]
+}
+
+# Insert the corresponding activity.
+final.selected$activity <- activity.map[
+  read.table("train/y_train.txt")[, 1]
+]
+
+# Create a dummy data frame to hold the test data.
+dummy.data <- data.frame(
+  subject = read.table("test/subject_test.txt")[, 1]
+)
+
+# Get the test data; this may take a few seconds.
+x.data <- read.table("test/X_test.txt", stringsAsFactors = FALSE)
+
+# Keep the data of interest.
+for (i in 1:561) {
+  if (to.be.included[i])
+    dummy.data[variable.names[i]] <- x.data[, i]
+}
+
+# Insert the corresponding activity.
+dummy.data$activity <- activity.map[
+  read.table("test/y_test.txt")[, 1]
+]
+
+# Bind the two data sets together.
+final.selected <- rbind(final.selected, dummy.data)
+
+# Clean up.
+rm(dummy.data)
+
+# Create a second, tidy data set with the average for each variable
+# by activity and subject.
+
+variable.names <- variable.names[to.be.included]
+
+final.data <- data.frame()
+invisible(
+  by(
+    final.selected, 
+    list(final.selected$subject, final.selected$activity), 
+    function(df) {
+      dummy.row <- data.frame(subject = df$subject[1])
+      for (v in variable.names) 
+        dummy.row[v] <- mean(df[v])
+      dummy.row$activity <- df$activity[1]
+      final.data <<- rbind(final.data, dummy.row)
+    }
+  )
+)
